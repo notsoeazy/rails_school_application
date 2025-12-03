e584a263bc0b86c0dfb6bf950759f8ae551d36bb
notsoeazy <gonzalesjohnezekiel35@gmail.com>
2025-12-03 14:32:40 +0800

    edit: school manager navbar and refactored the svg icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def inline_svg(filename, options = {})
+    file = Rails.root.join('app', 'assets', 'images', filename)
+    
+    return unless File.exist?(file)
+
+    svg = File.read(file).html_safe
+    
+    if options[:class].present?
+      svg = svg.to_s.sub('>', " class='#{options[:class]}'>").html_safe
+    end
+
+    svg
+  end
 end
